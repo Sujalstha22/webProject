13ad1bce0fa13e3ef94d6f422548e57b3932e640
Sujal Shrestha <shresthasujal309@gmail.com>
2025-06-24 23:13:16 +0545

    add features like footer ticketrate page and log in signup model

--- script.js
+++ script.js
@@ -71,3 +71,26 @@ document.addEventListener("DOMContentLoaded", function () {
   // Start the slider
   startSlider();
 });
+
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function switchModal(closeId, openId) {
+  closeModal(closeId);
+  openModal(openId);
+}
+
+// Close modal when clicking outside
+window.onclick = function (event) {
+  const modals = document.querySelectorAll(".modal");
+  modals.forEach((modal) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+};
